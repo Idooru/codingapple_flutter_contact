39f3affe2c8f40a68ebbcc11589ccddf9b82a8bc
idooru <shere1765@gmail.com>
2024-06-06 23:15:10 +0900

    use state

--- lib/body.dart
+++ lib/body.dart
@@ -2,24 +2,18 @@ import 'package:contact/profile.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-  const Body({super.key});
+  List<String> names;
+  Body({super.key, required this.names});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[300],
-      child: ListView(
-        children: [
-          Profile(
-            name: "홍길동",
-          ),
-          Profile(
-            name: "이승훈",
-          ),
-          Profile(
-            name: "박승훈",
-          ),
-        ],
+      child: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, i) {
+          return Profile(name: names[i]);
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,25 @@ import 'package:contact/header.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var names = ['김영숙', '홍길동', '피자집', '잉기도', '앙영숙'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: Header(),
-        body: Body(),
+        body: Body(names: names),
         bottomNavigationBar: Footer(),
       ),
     );
